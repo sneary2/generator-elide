81c9d90d8d08f68e6322fa4815006f130d6dbeb2
Phillip Quy Le <pitlv2109@gmail.com>
2017-04-09 22:15:33 -0500

    Added more options

--- generator-elide/generators/app/index.js
+++ generator-elide/generators/app/index.js
@@ -10,11 +10,9 @@ module.exports = class extends Generator {
 		// Calling the super constructor is important so our generator is correctly set up
 		super(args, opts);
 
-		// This method adds support for a `--example` flag
-        this.option('example');
-        // And you can then access it later; e.g.
-        // this.options.example? this._create_main(): this._prompting();
-		// this._create_main()
+        this.option('example');		// Option flag to generate an example
+		this.option('create');		// Option flag to create a new project
+		this.option('info');		// Option flag to show Elide boot info
 	}
 
 	_prompting() {
@@ -22,13 +20,14 @@ module.exports = class extends Generator {
 			type	: 'list',
 			name	: 'command',
 			message	: 'Choose an option below',
-			choices	: ['Try an example', 'Create new project', 'Other Stuff', 'Other Stuff']
+			choices	: ['Try an example', 'Create new project']
 		}]).then((answers) => {
 			if (answers.command === 'Try an example') {
 				// Generate an example
 				this._create_main();
 			}
 			else {
+				// Ask questions when creating a new project
 				console.log("Yo World");
 			}
 			// writing(answers);
@@ -111,113 +110,21 @@ module.exports = class extends Generator {
 	}
 
 	main() {
-		this.options.example? this._create_main(): this._prompting();
-
+		if (this.options.example) {
+			console.log("Generate an example");
+			this._create_main();
+		}
+		else if (this.options.create) {
+			console.log("Create a new project");
+		}
+		else if (this.options.info){
+			console.log("Elide Boot is a command line interface (CLI) for Yahoo! Elide libary");
+			console.log("Author: Deez Nuts");
+			console.log("Version: 1.0.0");
+			console.log("Contact: lame_email@suspicious-server.com");
+		}
+		else {
+			this._prompting();
+		}
 	}
-		// var project = {
-		// 	name: "com.yahoo.elide.example"
-		// }
-
-		// var file = project.name.split('.').join('/');
-		// // Create the main.java file
-		// this.fs.copyTpl(
-		// 	this.templatePath("main.java"),
-		// 	this.destinationPath("src/main/java/" + file + "/main.java"),
-		// 	{}
-		// );
-		// // Create the model folder
-		// this.fs.copyTpl(
-		// 	this.templatePath("test.txt"),
-		// 	this.destinationPath("src/test/java/" + file + "/models/test.txt"),
-		// 	{}
-		// );
-		// // Create the checks folder
-		// this.fs.copyTpl(
-		// 	this.templatePath("test.txt"),
-		// 	this.destinationPath("src/main/java/" + file + "/checks/test.txt"),
-		// 	{}
-		// );
-
-		// // Create the test models folder
-		// this.fs.copyTpl(
-		// 	this.templatePath("test.txt"),
-		// 	this.destinationPath("src/test/java/" + file + "/models/test.txt"),
-		// 	{}
-		// );
-		// // Create the test checks folder
-		// this.fs.copyTpl(
-		// 	this.templatePath("test.txt"),
-		// 	this.destinationPath("src/test/java/" + file + "/checks/test.txt"),
-		// 	{}
-		// );
-
-
-	// _model() {
-	// 	var new_model = model();
-	// 	this.prompt([{
-	// 		type	: 'input',
-	// 		name	: 'name',
-	// 		message : 'Model Name?'
-	// 	},{
-	// 		name	: 'type',
-	// 		message : 'Want to create a model perhaps?',
-	// 		type	: 'list',
-	// 		choices: [
-	// 			"String",
-	// 			"Int",
-	// 			"Short",
-	// 			"Float",
-	// 			"Double",
-	// 			"Long",
-	// 			"Long long",
-	// 			"Boolean",
-	// 			"Char"
-	// 		   ]
-	// 	}]).then((model) => {
-	// 		new_model.name = model.name;
-	// 		new_model.type = model.type
-	// 		this.log(model.name);
-	// 		this.log(model.type);
-	// 	});
-	// 	return new_model;
-	// }
-
-	// _create_models(model_name, models) {
-	// 	for(var i=0; i<models.length; i++) {
-	// 		this.fs.copyTpl(
-	// 			this.templatePath("model.java"),
-	// 			this.destinationPath("src/main/java/com/testapp/models/" + model_name + ".java"),
-	// 			{
-	// 				name 	: models[i].name,
-	// 				id_type	: models[i].type
-	// 			}
-	// 		);
-	// 	}
-	// }
-
-	// prompting() {
-	// 	var models = [];
-	// 	var model_name;
-
-	// 	this.prompt([{
-	// 		type	: 'confirm',
-	// 		name	: 'model',
-	// 		message : 'Want to create a model?'
-	// 	}]).then((answer) => {
-	// 		if(answer.model == true) {
-	// 			this.prompt([{
-	// 				type	: 'input',
-	// 				name	: 'model_name',
-	// 				message : 'Name of Model?'
-	// 			}]).then((answer) => {
-	// 				model_name = answer.model_name;
-	// 				models.append(_model(model_name, models));
-	// 			});
-	// 		} else {
-	// 			done = true;
-	// 		}
-	// 	});
-	// }
-
-	// 	_create_models(model_name, models);
 };
