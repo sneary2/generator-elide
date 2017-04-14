1f5178ba0d9253792946dd7a910898bcbde257dc
shaneneary <shanennn@gmail.com>
2017-04-14 10:05:21 -0500

    removed unnecessary files and updated gitignore

--- public/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Project Name: shane.n" << endl;
-    cout << "Cool Feature Enable?: false" << endl;
-    cout << "Database System: PostgreSQL" << endl;
-    return 0;
-}
