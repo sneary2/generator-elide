260c57b9433f797d05400bbcc2fd3ee240a462e1
Daniel Chen <danchen@yahoo-inc.com>
2017-04-23 16:12:23 -0500

    es6

--- generators/app/generator.js
+++ generators/app/generator.js
@@ -1,114 +1,113 @@
 // Generate an example
-function generateExampleProject(yo, project_name) {
+const generateExampleProject = (yo, projectName) => {
+  const file = projectName.split('.').join('/');
+  // Create the main.java file
+  yo.fs.copyTpl(
+    yo.templatePath('blog-example/Main.java'),
+    yo.destinationPath('elide-blog-example/src/main/java/' + file + '/Main.java'),
+    {}
+  );
+  yo.fs.copyTpl(
+    yo.templatePath('blog-example/ElideResourceConfig.java'),
+    yo.destinationPath('elide-blog-example/src/main/java/' + file + '/ElideResourceConfig.java'),
+    {}
+  );
 
-    var file = project_name.split('.').join('/');
-    // Create the main.java file
-    yo.fs.copyTpl(
-        yo.templatePath("blog-example/Main.java"),
-        yo.destinationPath("elide-blog-example/src/main/java/" + file + "/Main.java"),
-        {}
-    );
-    yo.fs.copyTpl(
-        yo.templatePath("blog-example/ElideResourceConfig.java"),
-        yo.destinationPath("elide-blog-example/src/main/java/" + file + "/ElideResourceConfig.java"),
-        {}
-    );
+  // Init the models folder blog-example
+  yo.fs.copyTpl(
+    yo.templatePath('blog-example/Comment.java'),
+    yo.destinationPath('elide-blog-example/src/main/java/' + file + '/models/Comment.java'),
+    {}
+  );
+  yo.fs.copyTpl(
+    yo.templatePath('blog-example/Post.java'),
+    yo.destinationPath('elide-blog-example/src/main/java/' + file + '/models/Post.java'),
+    {}
+  );
+  yo.fs.copyTpl(
+    yo.templatePath('blog-example/Role.java'),
+    yo.destinationPath('elide-blog-example/src/main/java/' + file + '/models/Role.java'),
+    {}
+  );
+  yo.fs.copyTpl(
+    yo.templatePath('blog-example/User.java'),
+    yo.destinationPath('elide-blog-example/src/main/java/' + file + '/models/User.java'),
+    {}
+  );
 
-    // Init the models folder blog-example
-    yo.fs.copyTpl(
-        yo.templatePath("blog-example/Comment.java"),
-        yo.destinationPath("elide-blog-example/src/main/java/" + file + "/models/Comment.java"),
-        {}
-    );
-    yo.fs.copyTpl(
-        yo.templatePath("blog-example/Post.java"),
-        yo.destinationPath("elide-blog-example/src/main/java/" + file + "/models/Post.java"),
-        {}
-    );
-    yo.fs.copyTpl(
-        yo.templatePath("blog-example/Role.java"),
-        yo.destinationPath("elide-blog-example/src/main/java/" + file + "/models/Role.java"),
-        {}
-    );
-    yo.fs.copyTpl(
-        yo.templatePath("blog-example/User.java"),
-        yo.destinationPath("elide-blog-example/src/main/java/" + file + "/models/User.java"),
-        {}
-    );
+  // Don't know what this is
+  yo.fs.copyTpl(
+    yo.templatePath('blog-example/hibernate.cfg.xml'),
+    yo.destinationPath('elide-blog-example/src/main/resources/hibernate.cfg.xml'),
+    {}
+  );
+  yo.fs.copyTpl(
+    yo.templatePath('blog-example/log4j2.xml'),
+    yo.destinationPath('elide-blog-example/src/main/resources/log4j2.xml'),
+    {}
+  );
 
-    // Don't know what this is
-    yo.fs.copyTpl(
-        yo.templatePath("blog-example/hibernate.cfg.xml"),
-        yo.destinationPath("elide-blog-example/src/main/resources/hibernate.cfg.xml"),
-        {}
-    );
-    yo.fs.copyTpl(
-        yo.templatePath("blog-example/log4j2.xml"),
-        yo.destinationPath("elide-blog-example/src/main/resources/log4j2.xml"),
-        {}
-    );
+  yo.fs.copyTpl(
+    yo.templatePath('blog-example/load_blog.sh'),
+    yo.destinationPath('elide-blog-example/src/scripts/load_blog.sh'),
+    {}
+  );
 
-    yo.fs.copyTpl(
-        yo.templatePath("blog-example/load_blog.sh"),
-        yo.destinationPath("elide-blog-example/src/scripts/load_blog.sh"),
-        {}
-    );
-
-    // create the pom files
-    yo.fs.copyTpl(
-        yo.templatePath("template-pom.xml"),
-        yo.destinationPath("elide-blog-example/pom.xml"),
-        {   
-            artifactId  : "elide-blog-example",
-            groupId     : "com.yahoo.elide",
-            name        : "Elide Example: Hibernate5 API with Security",
-            version     : "3.0.5-SNAPSHOT",
-            description : "lide example using javax.persistence, MySQL and Elide Security"
-        }
-    );
+  // create the pom files
+  yo.fs.copyTpl(
+    yo.templatePath('template-pom.xml'),
+    yo.destinationPath('elide-blog-example/pom.xml'),
+    {
+      artifactId  : 'elide-blog-example',
+      groupId     : 'com.yahoo.elide',
+      name        : 'Elide Example: Hibernate5 API with Security',
+      version     : '3.0.5-SNAPSHOT',
+      description : 'lide example using javax.persistence, MySQL and Elide Security'
+    }
+  );
 }
 
-function generateNewProject(yo, project_name, package_name, pom_obj) {
-    var file = package_name.split('.').join('/');
-    // Create the main.java file
-    yo.fs.copyTpl(
-        yo.templatePath("blog-example/Main.java"),
-        yo.destinationPath(project_name + "/src/main/java/" + file + "/Main.java"),
-        {}
-    );
-    yo.fs.copyTpl(
-        yo.templatePath("blog-example/ElideResourceConfig.java"),
-        yo.destinationPath(project_name + "/src/main/java/" + file + "/ElideResourceConfig.java"),
-        {}
-    );
+const generateNewProject = (yo, projectName, package_name, pom_obj) => {
+  const file = package_name.split('.').join('/');
+  // Create the main.java file
+  yo.fs.copyTpl(
+    yo.templatePath('blog-example/Main.java'),
+    yo.destinationPath(projectName + '/src/main/java/' + file + '/Main.java'),
+    {}
+  );
+  yo.fs.copyTpl(
+    yo.templatePath('blog-example/ElideResourceConfig.java'),
+    yo.destinationPath(projectName + '/src/main/java/' + file + '/ElideResourceConfig.java'),
+    {}
+  );
 
-    // Don't know what this is
-    yo.fs.copyTpl(
-        yo.templatePath("blog-example/hibernate.cfg.xml"),
-        yo.destinationPath(project_name + "/src/resources/hibernate.cfg.xml"),
-        {}
-    );
-    yo.fs.copyTpl(
-        yo.templatePath("blog-example/log4j2.xml"),
-        yo.destinationPath(project_name + "/src/resources/log4j2.xml"),
-        {}
-    );
+  // Don't know what this is
+  yo.fs.copyTpl(
+    yo.templatePath('blog-example/hibernate.cfg.xml'),
+    yo.destinationPath(projectName + '/src/resources/hibernate.cfg.xml'),
+    {}
+  );
+  yo.fs.copyTpl(
+    yo.templatePath('blog-example/log4j2.xml'),
+    yo.destinationPath(projectName + '/src/resources/log4j2.xml'),
+    {}
+  );
 
-    yo.fs.copyTpl(
-        yo.templatePath("blog-example/load_blog.sh"),
-        yo.destinationPath(project_name + "/src/scripts/load_blog.sh"),
-        {}
-    );
+  yo.fs.copyTpl(
+    yo.templatePath('blog-example/load_blog.sh'),
+    yo.destinationPath(projectName + '/src/scripts/load_blog.sh'),
+    {}
+  );
 
-    // create the pom files when creating new project
-    yo.fs.copyTpl(
-        yo.templatePath("template-pom.xml"),
-        yo.destinationPath(project_name + "/pom.xml"),
-        pom_obj
-    );
+  // create the pom files when creating new project
+  yo.fs.copyTpl(
+    yo.templatePath('template-pom.xml'),
+    yo.destinationPath(projectName + '/pom.xml'),
+    pom_obj
+  );
 }
 
 module.exports = {
-    generateExampleProject: generateExampleProject,
-    generateNewProject: generateNewProject
+  generateExampleProject,
+  generateNewProject
 }
