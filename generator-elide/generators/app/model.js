6f1c5cd7556c01d373ca89da8a7343bd27eb8d13
shaneneary <shanennn@gmail.com>
2017-04-23 13:36:34 -0500

    added support for adding multiple models

--- generator-elide/generators/app/model.js
+++ generator-elide/generators/app/model.js
@@ -1,8 +1,6 @@
-
-
-
-
 var generator = require('./generator');
+
+var models = [];
 
 var new_model_attributes = {name: "", schemas: [] };
 
@@ -37,41 +35,73 @@ function schema_promt(yo, project_name, package_name) {
 			if (response.continue === true) {
 				// console.log("yes");
 				schema_promt(yo, project_name, package_name);
-			}
-			else {
-				create_model(yo, project_name, package_name);
+			} else {
+				models.push(new_model_attributes);
+				new_model_attributes = {name: "", schemas: [] };
+				// create_models(yo, project_name, package_name);
+				yo.prompt([{
+						type	: 'confirm',
+						name	: 'add_another',
+						message : 'Add another model?'
+				}]).then((answer) => {
+					if(answer.add_another) {
+						model_prompt(yo, project_name, package_name);
+					} else {
+						create_models(yo, project_name, package_name)
+					}
+				})
 			}
 		});
 	});
 }
 
+// function continue_making_models(yo, project_name, package_name) {
+// 	yo.prompt([{
+// 			type	: 'confirm',
+// 			name	: 'add_another',
+// 			message : 'Add another model?'
+// 	},]).then((answer) => {
+// 		if(answer.add_another) {
+// 			model_prompt
+// 		}
+// 	}
+// }
+
 function model_prompt(yo, project_name, package_name) {
-	return yo.prompt([{
-		type	: 'input',
-		name	: 'name',
-		message : 'Model Name?'
-	},]).then((model) => {
+	// var done_adding_models = false;
+	// while(!done_adding_models) {
+		yo.prompt([{
+			type	: 'input',
+			name	: 'name',
+			message : 'Model Name?'
+		},]).then((model) => {
 
-		new_model_attributes.name = model.name;
+			new_model_attributes.name = model.name;
 
-		schema_promt(yo, project_name, package_name);
-
-	});
+			schema_promt(yo, project_name, package_name);
+		});
+	// }
 }
 
-function create_model(yo, project_name, package_name) {
+function create_models(yo, project_name, package_name) {
 	var file = package_name.split('.').join('/')
-	yo.fs.copyTpl(
-		yo.templatePath("model.java"),
-		yo.destinationPath(project_name + "/src/main/java/" + file + "/models/" + new_model_attributes.name + ".java"),
-		new_model_attributes
+	models.forEach(
+		function(model) {
+			yo.fs.copyTpl(
+				yo.templatePath("model.java"),
+				yo.destinationPath(project_name + "/src/main/java/" + file + "/models/" + model.name + ".java"),
+				model
+			);
+		}
 	);
+	
     generator.generate_new_project(yo, project_name, package_name);
 }
 
 module.exports = {
-    schema_promt: schema_promt,
+    // schema_promt: schema_promt,
     model_prompt: model_prompt,
-    create_model: create_model,
+    // create_models: create_models,
     new_model_attributes: new_model_attributes
 }
+
