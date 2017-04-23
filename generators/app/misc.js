25e5da5fd76e88fcbe10dc36428971adde0b9f13
Phillip Quy Le <pitlv2109@gmail.com>
2017-04-23 16:16:15 -0500

    Updated misc.js

--- generators/app/misc.js
+++ generators/app/misc.js
@@ -1,21 +1,21 @@
 // Show info
 function showInfo() {
-    const eb1 = "_____ _ _   _        _____         _   "
-    const eb2 = "|   __| |_|_| |___   | __  |___ ___| |_ "
-    const eb3 = "|   __| | | . | -_|  | __ -| . | . |  _|"
-    const eb4 = "|_____|_|_|___|___|  |_____|___|___|_|"
-    const eb5 = "                                  v.1.0"
+    const eb1 = '_____ _ _   _        _____         _   '
+    const eb2 = '|   __| |_|_| |___   | __  |___ ___| |_ '
+    const eb3 = '|   __| | | . | -_|  | __ -| . | . |  _|'
+    const eb4 = '|_____|_|_|___|___|  |_____|___|___|_|'
+    const eb5 = '                                   v.1.0'
     console.log(eb1);
     console.log(eb2);
     console.log(eb3);
     console.log(eb4);
     console.log(eb5);
-    console.log("_______________________________________________________________");
-    console.log("|Elide Boot is a command line interface (CLI) for Yahoo! Elide|");
-    console.log("|Author: Deez Nuts                                            |");
-    console.log("|Version: 1.0.0                                               |");
-    console.log("|Contact: lame_email@suspicious-server.com                    |");
-    console.log("---------------------------------------------------------------");
+    console.log('_______________________________________________________________');
+    console.log('|Elide Boot is a command line interface (CLI) for Yahoo! Elide|');
+    console.log('|Author: Deez Nuts                                            |');
+    console.log('|Version: 1.0.0                                               |');
+    console.log('|Contact: lame_email@suspicious-server.com                    |');
+    console.log('---------------------------------------------------------------');
 }
 
 module.exports = {
